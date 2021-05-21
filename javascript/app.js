66a15b20eec362df19aac380e49b07dc6985496f
akhilesoni <akhileshshroff@gmail.com>
2021-05-21 11:47:10 +0530

    new commit

--- javascript/app.js
+++ javascript/app.js
@@ -5,12 +5,12 @@ var name_svg = document.getElementById('main_name')
 var menu = document.getElementById('menu')
 
 var pg = document.getElementById('pg')
-
+var boy = document.getElementById('boy')
 
 var mobile = document.getElementById('mobile')
 var exit = document.getElementById('exit')
 var open = document.getElementById('open_mobile')
-
+var connectname = document.getElementById('connect_name')
 cover.addEventListener('mousemove',(e)=>{
     image.style.transform = "translateX("+ - e.offsetX/30+"px) translateY("+ - e.offsetY/30+"px) "
     name_svg.style.transform = "translateX("+ - e.offsetX/60+"px) translateY("+ - e.offsetY/60+"px) rotateZ(-10deg) "
@@ -35,7 +35,7 @@ exit.addEventListener('click',()=>{
 
 })
 window.addEventListener('scroll',()=>{
-    if(window.scrollY<window.screen.height){
+    if(window.scrollY<window.screen.height-100){
         pg.style.opacity = 0
         pg.style.transform = "translateY(100px)"
         menu.style.visibility = 'visible'
@@ -50,4 +50,26 @@ window.addEventListener('scroll',()=>{
 
 
     }
+
+    if(window.scrollY<2*window.screen.height){
+        boy.style.opacity = 0
+        boy.style.transform = "translateX(100px)"
+
+   
+        connectname.style.opacity = 0
+        connectname.style.transform = "translateX(100px)"
+    }else{
+        boy.style.opacity = 1
+        boy.style.transform = "translateX(0px)"
+
+
+
+
+        connectname.style.opacity = 1
+        connectname.style.transform = "translateX(0px)"
+    }
+
+
+
+
 })
